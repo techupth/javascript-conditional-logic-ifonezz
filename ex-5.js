e950e74d3dc3d4bfe5f485f3087c0c391e2cdfb4
Thanawan Wongchanasri <thanawan@orcsoft.co.th>
2024-04-06 23:40:38 +0700

    submit javascript Conditional Logix Assignment

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,18 @@
 function calculateStudentGrade(score) {
-  // Start coding here
+   switch (true) {
+     case score >= 90 && score <= 100:
+      return "A";
+     case score >= 80 && score <= 89:
+      return "B";
+     case score >= 70 && score <= 79:
+      return "C";
+     case score >= 60 && score <= 69:
+      return "D";
+     case score >= 50 && score <= 59:
+      return "F";
+     default :
+      return "F";
+   }
 }
 
 // Example case
